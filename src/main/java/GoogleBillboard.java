d6f7250494ec552dc600ade6c9f53a1186bb245b
Kyle Yuen <103063168+RandomHouseGuest@users.noreply.github.com>
2024-01-19 09:58:47 -0800

    GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -5,11 +5,9 @@ public class GoogleBillboard {
 	public static void main(final String[] args) {
 		String digits = e.substring(2, 12);
 		double dnum = Double.parseDouble(digits);
-		System.out.println(dnum);
 		for(int i = 2; i < e.length(); i++){
 			digits = e.substring(i, i+11);
 			dnum = Double.parseDouble(digits);
-			System.out.println(isPrime(dnum));
 			if(isPrime(dnum) == true){
 				break;
 			}
